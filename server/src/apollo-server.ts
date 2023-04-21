6484ea0b873122d2082fb0137a03042ed76b34c3
Mherzog <matthewherzog4@gmail.com>
2023-04-21 14:44:26 -0400

    breaking up resolvers

--- server/src/apollo-server.ts
+++ server/src/apollo-server.ts
@@ -3,34 +3,28 @@ import { ApolloServer, ExpressContext, gql } from 'apollo-server-express';
 import * as express from 'express';
 import { Server } from 'http';
 import Db from './db';
+import { GraphQLFileLoader } from '@graphql-tools/graphql-file-loader';
+import { loadSchemaSync } from '@graphql-tools/load';
+import { addResolversToSchema } from '@graphql-tools/schema';
+import { GRAPHQL_SCHEMA_PATH } from './constants';
+import resolvers from "./resolvers"
+
+const SCHEMA = loadSchemaSync(GRAPHQL_SCHEMA_PATH, {
+  loaders: [new GraphQLFileLoader()],
+});
+
+
 export async function createApolloServer(
-  _db: Db,
+  db: Db,
   httpServer: Server,
   app: express.Application
 ): Promise<ApolloServer<ExpressContext>> {
-  const typeDefs = gql`
-    type Query {
-      currentUser: User!
-      suggestions: [Suggestion!]!
-    }
-    type User {
-      id: String!
-      name: String!
-      handle: String!
-      coverUrl: String!
-      avatarUrl: String!
-      createdAt: String!
-      updatedAt: String!
-    }
-    type Suggestion {
-      name: String!
-      handle: String!
-      avatarUrl: String!
-      reason: String!
-    }
-  `;
   const server = new ApolloServer({
-    typeDefs,
+    schema: addResolversToSchema({
+      schema: SCHEMA,
+      resolvers,
+    }),
+    context: () => ({ db }),
     plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
   });
   await server.start();
---
+++ server/src/resolvers.ts
@@ -0,0 +1,9 @@
+import Query from './resolvers/Query';
+import Db from './db';
+import { Resolvers } from './resolvers-types.generated';
+
+const resolvers = {
+  Query,
+};
+
+export default resolvers;
